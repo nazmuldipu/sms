cbf9a415ca942222c1a95f8322f2ee00a484213d
Nazmul Alam <nazmuldipu@gmail.com>
2018-03-11 19:58:46 +0600

    Auto-generated commit

---
+++ 0.5364032ca80496f35ad0.chunk.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],{GazG:function(t,s,n){"use strict";n.d(s,"a",function(){return i});var i=function(t,s,n,i,e,a,h,o,c,u,m){this.id=t,this.companyName=s,this.companyNameBangla=n,this.companyAddress=i,this.contactPerson=e,this.telephone=a,this.webAddress=h,this.maximumNumberOfStudent=o,this.country=c,this.smsQuota=u,this.perMonthValue=m}}});
